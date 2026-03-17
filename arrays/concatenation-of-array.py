7d77ead5d031051ecf693cdf87f5a45ff756af1f
SakshiG13arg <sakshigarg0913@gmail.com>
2026-03-17 15:53:49 +0530

    Add Solution class with getConcatenation method
    Implemented a class method to concatenate an array twice.

--- arrays/concatenation-of-array.py
+++ arrays/concatenation-of-array.py
@@ -1,1 +1,10 @@
-
+class Solution:
+    def getConcatenation(self, nums: List[int]) -> List[int]:
+        # create an empty list
+        ans = []
+        # 2 because it has to be appended two times
+        for i in range(2):
+            # iterate through every number in nums
+            for n in nums:
+                ans.append(n)
+        return ans
