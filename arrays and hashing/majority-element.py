59d2e6d60d84d63407693372a6514fd5235fb679
SakshiG13arg <sakshigarg0913@gmail.com>
2026-03-17 16:35:40 +0530

    Add majorityElement method to Solution class
    Implement majorityElement method to find the majority element by sorting.

--- arrays and hashing/majority-element.py
+++ arrays and hashing/majority-element.py
@@ -1,1 +1,6 @@
-
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        # after sorting the numbers the maximum occuring element will come at the middle
+        nums.sort()
+        # prints the middle value
+        return nums[len(nums) // 2]
