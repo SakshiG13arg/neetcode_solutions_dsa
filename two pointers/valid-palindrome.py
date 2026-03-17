ff2a71641a64c507fc89ee6559b3f42444e98d07
SakshiG13arg <sakshigarg0913@gmail.com>
2026-03-17 16:45:39 +0530

    Add isPalindrome method to Solution class
    Implement palindrome check for alphanumeric strings.

--- two pointers/valid-palindrome.py
+++ two pointers/valid-palindrome.py
@@ -1,1 +1,15 @@
-
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        modified = ""
+        for c in s:
+            if c.isalnum():
+                modified += c.lower()
+        reversed_str = modified[::-1]
+        return modified == reversed_str
+        l, r = 0, reversed_str - 1
+        while l < len(modified) and r >= 0:
+            if modified[l] != reversed_str[r]:
+                return False
+            l += 1
+            r -= 1
+        return True
