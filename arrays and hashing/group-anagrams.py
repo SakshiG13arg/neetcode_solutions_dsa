87392a6ec8ed7d300df7f811d2638b200acbac0d
SakshiG13arg <sakshigarg0913@gmail.com>
2026-03-17 16:33:29 +0530

    Add groupAnagrams method to group anagrams
    Implemented a method to group anagrams using a default dictionary to store character counts as keys.

--- arrays and hashing/group-anagrams.py
+++ arrays and hashing/group-anagrams.py
@@ -1,1 +1,16 @@
-
+from collections import defaultdict
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        # deafult dictionary is made to make a default dictionary to store key and values
+        anagrams_dict = defaultdict(list)
+        for s in strs:
+            # giving all the alphabets a count 0
+            count = [0] * 26
+            for c in s:
+                # gives the number of times an alphabet occurs
+                count[ord(c) - ord('a')] += 1
+            # converts the key to tuple because a dictionary cant be stored in a list
+            key = tuple(count)
+            # append the string in the dictionary that is an anagram
+            anagrams_dict[key].append(s)
+        return list(anagrams_dict.values())
