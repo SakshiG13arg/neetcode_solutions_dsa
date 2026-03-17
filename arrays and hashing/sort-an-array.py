73bd1e5726a7342b1ec95d9593e9bb1f695970e5
SakshiG13arg <sakshigarg0913@gmail.com>
2026-03-17 16:39:03 +0530

    Implement two sorting methods in sort-an-array.py

--- arrays and hashing/sort-an-array.py
+++ arrays and hashing/sort-an-array.py
@@ -1,1 +1,41 @@
+# first method
+class Solution:
+    def sortArray(self, nums: List[int]) -> List[int]:
+        def merge(arr, L, M, R):
+            left,right = arr[L:M + 1], arr[M + 1: R + 1]
+            i, j, k = L, 0, 0
+            while j < len(left) and k < len(right):
+                if left[j] <= right[k]:
+                    arr[i] = left[j]
+                    j += 1
+                else:
+                    arr[i] = right[k]
+                    k += 1
+                i += 1
+            while j < len(left):
+                arr[i] = left[j]
+                j += 1
+                i += 1
+            while k< len(right):
+                arr[i] = right[k]
+                k += 1
+                i += 1
+        def mergeSort(arr, l, r):
+            if l >= r:
+                return
+            m =(l + r) // 2
+            mergeSort(arr,l, m)
+            mergeSort(arr,m + 1, r)
+            merge(arr, l, m, r)
+        mergeSort(nums, 0, len(nums) - 1)
+        return nums
 
+# second method
+class Solution:
+    def sortArray(self, nums: List[int]) -> List[int]:
+        n = len(nums)
+        for i in range(n):
+            for j in range(0, n - i - 1):
+                if nums[j] > nums[j + 1]:
+                    nums[j], nums[j + 1] = nums[j + 1], nums[j]
+        return nums
