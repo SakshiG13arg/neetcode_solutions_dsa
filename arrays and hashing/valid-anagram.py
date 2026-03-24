d8dbc43152769ffaeea53ec6c02f928872cebd98
SakshiG13arg <sakshigarg0913@gmail.com>
2026-03-24 15:36:55 +0530

    Add anagram checking function in valid-anagram.py
    Implemented a solution to check if two strings are anagrams by comparing character counts.

--- arrays and hashing/valid-anagram.py
+++ arrays and hashing/valid-anagram.py
@@ -1,1 +1,14 @@
-
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        # if lenghts are not equal then automatically it is not an anagram
+        if len(s) != len(t):
+            return False
+        countS, countT = {}, {}
+        for i in range(len(s)):
+            # return 0 if any value is not in the string
+            countS[s[i]] = 1 + countS.get(s[i], 0)
+            countT[t[i]] = 1 + countT.get(t[i], 0)
+        for c in countS:
+            if countS[c] != countT.get(c,0):
+                return False
+        return True
