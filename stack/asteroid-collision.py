215ba0066093c5b56cc9ce53a28a623e4082a91b
SakshiG13arg <sakshigarg0913@gmail.com>
2026-03-25 16:54:14 +0530

    Add asteroid collision solution using stack
    Implement asteroid collision logic using a stack to handle collisions between asteroids moving in opposite directions.

--- stack/asteroid-collision.py
+++ stack/asteroid-collision.py
@@ -1,1 +1,19 @@
-
+class Solution(object):
+    def asteroidCollision(self, asteroids):
+        """
+        :type asteroids: List[int]
+        :rtype: List[int]
+        """
+        stack = []
+        for a in asteroids:
+            while stack and a < 0 and stack[-1] > 0:
+                if abs(a) > abs(stack[-1]):
+                    stack.pop()
+                elif abs(a) < abs(stack[-1]):
+                    a = 0
+                else:
+                    a = 0
+                    stack.pop()
+            if a:
+                stack.append(a)
+        return stack
