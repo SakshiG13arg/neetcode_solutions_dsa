d500b28e3a1f795b4c75adeba84bb841a34e2c7c
SakshiG13arg <sakshigarg0913@gmail.com>
2026-03-25 20:39:38 +0530

    Add dailyTemperatures function to calculate waits
    Implement daily temperature calculation using a stack.

--- stack/daily-temperatures.py
+++ stack/daily-temperatures.py
@@ -1,1 +1,15 @@
-
+class Solution(object):
+    def dailyTemperatures(self, temperatures):
+        """
+        :type temperatures: List[int]
+        :rtype: List[int]
+        """
+        stack = []
+        n = len(temperatures)
+        res = [0] * n
+        for i in range(n):
+            while stack and temperatures[i] > temperatures[stack[-1]]:
+                idx = stack.pop()
+                res[idx] = i - idx
+            stack.append(i)
+        return res
