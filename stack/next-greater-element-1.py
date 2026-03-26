3e5451f8733c055c00c86559f110059062348811
SakshiG13arg <sakshigarg0913@gmail.com>
2026-03-26 16:22:34 +0530

    Add nextGreaterElement method to Solution class
    Implement nextGreaterElement method to find the next greater elements for nums1 in nums2.

--- stack/next-greater-element-1.py
+++ stack/next-greater-element-1.py
@@ -1,1 +1,16 @@
-
+class Solution:
+    def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        nums1Idx = {}
+        for i in range(len(nums1)):
+            nums1Idx[nums1[i]] = i
+        res = [-1] * len(nums1)
+        stack = []
+        for i in range(len(nums2)):
+            curr = nums2[i]
+            while stack and curr > stack[-1]:
+                val = stack.pop()
+                idx = nums1Idx[val]
+                res[idx] = curr
+            if curr in nums1Idx:
+                stack.append(curr)
+        return res
