b7bcdaae1f32dba96173e6339db181701434a868
SakshiG13arg <sakshigarg0913@gmail.com>
2026-03-27 17:41:18 +0530

    Update buildings-with-an-ocean-view.py

--- stack/buildings-with-an-ocean-view.py
+++ stack/buildings-with-an-ocean-view.py
@@ -1,1 +1,8 @@
-
+class Solution:
+    def findBuildings(self, heights: List[int]) -> List[int]:
+        stack = []
+        for i in range(len(heights)):
+            while stack and heights[i] >= heights[stack[-1]]:
+                stack.pop()
+            stack.append(i)
+        return stack
