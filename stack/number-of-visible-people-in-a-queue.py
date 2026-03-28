0c33c5d0f28509673970a8b98d90d5e117889c91
SakshiG13arg <sakshigarg0913@gmail.com>
2026-03-28 17:13:15 +0530

    Update number-of-visible-people-in-a-queue.py

--- stack/number-of-visible-people-in-a-queue.py
+++ stack/number-of-visible-people-in-a-queue.py
@@ -1,1 +1,13 @@
-
+class Solution:
+    def canSeePersonsCount(self, heights: List[int]) -> List[int]:
+        n = len(heights)
+        res = [0] * n
+        stack = []
+        for i in range(n):
+            h = heights[i]
+            while stack and heights[stack[-1]] < h:
+                res[stack.pop()] += 1
+            if stack:
+                res[stack[-1]] += 1
+            stack.append(i)
+        return res
