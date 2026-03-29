68fbef358651d75349f5d54dfa601615894386bf
SakshiG13arg <sakshigarg0913@gmail.com>
2026-03-29 16:17:42 +0530

    Implement binary search for package shipping capacity

--- binary search/capacity-to-ship-packages-within-d-days.py
+++ binary search/capacity-to-ship-packages-within-d-days.py
@@ -1,1 +1,29 @@
-
+class Solution(object):
+    def shipWithinDays(self, weights, days):
+        """
+        :type weights: List[int]
+        :type days: int
+        :rtype: int
+        """
+        l = max(weights)
+        r = sum(weights)
+        res = r
+        def canShip(cap):
+            ships = 1
+            currCap = cap
+            for w in weights:
+                if currCap - w < 0:
+                    ships += 1
+                    if ships > days:
+                        return False
+                    currCap = cap
+                currCap -= w
+            return True
+        while l <= r:
+            cap = (l + r) // 2
+            if canShip(cap):
+                res = min(res, cap)
+                r = cap - 1
+            else:
+                l = cap + 1
+        return res
