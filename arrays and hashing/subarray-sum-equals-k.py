62475bba6b9bf4211271939fb23e4f006f8a02cc
SakshiG13arg <sakshigarg0913@gmail.com>
2026-04-03 12:49:52 +0530

    Implement subarray sum equals k function

--- arrays and hashing/subarray-sum-equals-k.py
+++ arrays and hashing/subarray-sum-equals-k.py
@@ -1,1 +1,11 @@
-
+class Solution:
+    def subarraySum(self, nums: List[int], k: int) -> int:
+        curSum = 0
+        res = 0
+        prefixSums = {0 : 1}
+        for num in nums:
+            curSum += num
+            diff = curSum - k
+            res += prefixSums.get(diff, 0)
+            prefixSums[curSum] = 1 + prefixSums.get(curSum, 0)
+        return res
