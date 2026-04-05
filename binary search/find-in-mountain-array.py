dd9c899a7f60f7d4ed185609b2145f2ac4456776
SakshiG13arg <sakshigarg0913@gmail.com>
2026-04-05 19:46:35 +0530

    Implement findInMountainArray method for searching

--- binary search/find-in-mountain-array.py
+++ binary search/find-in-mountain-array.py
@@ -1,1 +1,43 @@
-
+class Solution:
+    def findInMountainArray(self, target: int, mountainArr: 'MountainArray') -> int:
+        length = mountainArr.length()
+        # Find Peak
+        l = 1
+        r = length - 2
+        while l <= r:
+            m = (l + r) // 2
+            left = mountainArr.get(m - 1)
+            mid = mountainArr.get(m)
+            right = mountainArr.get(m + 1)
+            if left < mid < right:
+                l = m + 1
+            elif left > mid > right:
+                r = m - 1
+            else:
+                break
+        peak = m
+        # Search left portion
+        l = 0
+        r = peak - 1
+        while l <= r:
+            m = (l + r) // 2
+            val = mountainArr.get(m)
+            if val < target:
+                l = m + 1
+            elif val > target:
+                r = m - 1
+            else:
+                return m
+        # search right portion
+        l = peak
+        r = length - 1
+        while l <= r:
+            m = (l + r) // 2
+            val = mountainArr.get(m)
+            if val > target:
+                l = m + 1
+            elif val < target:
+                r = m - 1
+            else:
+                return m
+        return -1
