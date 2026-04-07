534f48bdb00211cd95293a810014b032d6b0a6cd
SakshiG13arg <sakshigarg0913@gmail.com>
2026-04-07 13:06:57 +0530

    Implement maxSubArray function for maximum subarray sum

--- greedy/maximum-subarray.py
+++ greedy/maximum-subarray.py
@@ -1,1 +1,10 @@
-
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        curSum = 0
+        maxSub = nums[0]
+        for num in nums:
+            if curSum < 0:
+                curSum = 0
+            curSum += num
+            maxSub = max(maxSub, curSum)
+        return maxSub
