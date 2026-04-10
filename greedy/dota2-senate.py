5f935ae55bb83f7b5d3e41fd7a10964c0118d702
SakshiG13arg <sakshigarg0913@gmail.com>
2026-04-10 15:14:52 +0530

    Update dota2-senate.py

--- greedy/dota2-senate.py
+++ greedy/dota2-senate.py
@@ -1,1 +1,18 @@
-
+class Solution:
+    def predictPartyVictory(self, senate: str) -> str:
+        D = deque()
+        R = deque()
+        n = len(senate)
+        for i, c in enumerate(senate):
+            if c == "R":
+                R.append(i)
+            else:
+                D.append(i)
+        while D and R:
+            dTurn = D.popleft()
+            rTurn = R.popleft()
+            if rTurn < dTurn:
+                R.append(rTurn + n)
+            else:
+                D.append(dTurn + n)
+        return "Radiant" if R else "Dire"
