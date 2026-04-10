29229f1a63c6a9180df6fda2646e74ae781a4ecf
SakshiG13arg <sakshigarg0913@gmail.com>
2026-04-10 15:27:52 +0530

    Add mergeTriplets method to Solution class
    Implement mergeTriplets method to check if triplets can form the target.

--- greedy/merge-triplets-to-form-target.py
+++ greedy/merge-triplets-to-form-target.py
@@ -1,1 +1,15 @@
-
+class Solution:
+    def mergeTriplets(self, triplets: List[List[int]], target: List[int]) -> bool:
+        x = False
+        y = False
+        z = False
+        for t in triplets:
+            if t[0] == target[0] and t[1] <= target[1] and t[2] <= target[2]:
+                x = True
+            if t[0] <= target[0] and t[1] == target[1] and t[2] <= target[2]:
+                y = True
+            if t[0] <= target[0] and t[1] <= target[1] and t[2] == target[2]:
+                z = True
+            if x and y and z:
+                return True
+        return False
