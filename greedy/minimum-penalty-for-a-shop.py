58d75df52adc54d8985ab32a18bab504c558b8f2
SakshiG13arg <sakshigarg0913@gmail.com>
2026-04-15 15:54:59 +0530

    Implement bestClosingTime method for penalty calculation

--- greedy/minimum-penalty-for-a-shop.py
+++ greedy/minimum-penalty-for-a-shop.py
@@ -1,1 +1,15 @@
-
+class Solution(object):
+    def bestClosingTime(self, customers):
+        """
+        :type customers: str
+        :rtype: int
+        """
+        res = 0
+        penalty = 0
+        minPenalty = 0
+        for i, c in enumerate(customers):
+            penalty += 1 if c == "Y" else -1
+            if penalty > minPenalty:
+                minPenalty = penalty
+                res = i + 1
+        return res
