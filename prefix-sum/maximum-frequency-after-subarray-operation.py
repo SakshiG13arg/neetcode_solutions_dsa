5d7fabdb8bb8c78db803337c531c2242ef5b8930
SakshiG13arg <sakshigarg0913@gmail.com>
2026-04-17 18:45:01 +0530

    Add maxFrequency method to Solution class
    Implement maxFrequency method to calculate maximum frequency after subarray operations.

--- prefix-sum/maximum-frequency-after-subarray-operation.py
+++ prefix-sum/maximum-frequency-after-subarray-operation.py
@@ -1,1 +1,16 @@
-
+class Solution:
+    def maxFrequency(self, nums: List[int], k: int) -> int:
+        countK = nums.count(k)
+        res = 0
+        for i in range(1, 51):
+            if i == k:
+                continue
+            count = 0
+            for num in nums:
+                if num == i:
+                    count += 1
+                if num == k:
+                    count -= 1
+                count = max(count, 0)
+                res = max(res, countK + count)
+        return res
