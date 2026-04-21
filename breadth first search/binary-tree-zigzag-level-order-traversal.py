5abbe73e3bc8162f5855c4cc923617ce5a145c37
SakshiG13arg <sakshigarg0913@gmail.com>
2026-04-21 15:34:09 +0530

    Implement zigzag level order traversal for binary tree

--- breadth first search/binary-tree-zigzag-level-order-traversal.py
+++ breadth first search/binary-tree-zigzag-level-order-traversal.py
@@ -1,1 +1,23 @@
-
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def zigzagLevelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        res = []
+        q = deque([root] if root else [])
+        while q:
+            size = len(q)
+            level = [0] * size
+            for i in range(size):
+                node = q.popleft()
+                idx = size - i - 1 if len(res) % 2 else i
+                level[idx] = node.val
+                if node.left:
+                    q.append(node.left)
+                if node.right:
+                    q.append(node.right)
+            res.append(level)
+        return res
