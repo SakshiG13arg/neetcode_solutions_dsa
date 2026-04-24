f9b2904546ef0a7775000c4743cef4020f96f2af
SakshiG13arg <sakshigarg0913@gmail.com>
2026-04-24 16:49:43 +0530

    Implement BFS for rotting oranges problem

--- breadth first search/rotting-fruit.py
+++ breadth first search/rotting-fruit.py
@@ -1,1 +1,25 @@
-
+class Solution:
+    def orangesRotting(self, grid: List[List[int]]) -> int:
+        q = collections.deque()
+        fresh = 0
+        time = 0
+        for r in range(len(grid)):
+            for c in range(len(grid[0])):
+                if grid[r][c] == 1:
+                    fresh += 1
+                if grid[r][c] == 2:
+                    q.append((r, c))
+        directions = [[0, 1], [0, -1], [1, 0], [-1, 0]]
+        while fresh > 0 and q:
+            length = len(q)
+            for i in range(length):
+                r, c = q.popleft()
+                for dr, dc in directions:
+                    nr = dr + r
+                    nc = dc + c
+                    if (nr in range(len(grid)) and nc in range(len(grid[0]))and grid[nr][nc] == 1):
+                        grid[nr][nc] = 2
+                        q.append((nr, nc))
+                        fresh -= 1
+            time += 1
+        return time if fresh == 0 else -1
