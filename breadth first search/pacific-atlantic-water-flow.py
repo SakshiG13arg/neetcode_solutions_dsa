c6d66d33abc274e12ea8c5926ce65cc5e1eacd9c
SakshiG13arg <sakshigarg0913@gmail.com>
2026-04-24 17:17:05 +0530

    Implement Pacific Atlantic Water Flow algorithm

--- breadth first search/pacific-atlantic-water-flow.py
+++ breadth first search/pacific-atlantic-water-flow.py
@@ -1,1 +1,33 @@
-
+class Solution:
+    def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
+        rows = len(heights)
+        cols = len(heights[0])
+        directions = [[0, 1], [0, -1], [1, 0], [-1, 0]]
+        pac = [[False] * cols for _ in range(rows)]
+        atl = [[False] * cols for _ in range(rows)]
+        def bfs(source, ocean):
+            q = deque(source)
+            while q:
+                r, c = q.popleft()
+                ocean[r][c] = True
+                for dr, dc in directions:
+                    nr = dr + r
+                    nc = dc + c
+                    if (0 <= nr < rows and 0 <= nc < cols and not ocean[nr][nc] and heights[nr][nc] >= heights[r][c]):
+                        q.append((nr, nc))
+        pacific = []
+        atlantic = []
+        for c in range(cols):
+            pacific.append((0, c))
+            atlantic.append((rows - 1, c))
+        for r in range(rows):
+            pacific.append((r, 0))
+            atlantic.append((r, cols - 1))
+        bfs(pacific, pac)
+        bfs(atlantic, atl)
+        res = []
+        for r in range(rows):
+            for c in range(cols):
+                if pac[r][c] and atl[r][c]:
+                    res.append([r, c])
+        return res
