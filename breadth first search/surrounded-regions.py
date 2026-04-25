cab8c6c75a5bb451e926e7f7acbef0d6379f8dd3
SakshiG13arg <sakshigarg0913@gmail.com>
2026-04-25 15:47:39 +0530

    Implement BFS to capture surrounded regions

--- breadth first search/surrounded-regions.py
+++ breadth first search/surrounded-regions.py
@@ -1,1 +1,27 @@
-
+class Solution:
+    def solve(self, board: List[List[str]]) -> None:
+        rows = len(board)
+        cols = len(board[0])
+        directions = [[0, 1], [0, -1], [1, 0], [-1, 0]]
+        def capture():
+            q = deque()
+            for r in range(rows):
+                for c in range(cols):
+                    if (r == 0 or r == rows - 1 or c == 0 or c == cols - 1) and board[r][c] == "O":
+                        q.append((r, c))
+            while q:
+                r, c = q.popleft()
+                if board[r][c] == "O":
+                    board[r][c] = "T"
+                    for dr, dc in directions:
+                        nr = r + dr
+                        nc = c + dc
+                        if 0 <= nr < rows and 0 <= nc < cols:
+                            q.append((nr, nc))
+        capture()
+        for r in range(rows):
+            for c in range(cols):
+                if board[r][c] == "O":
+                    board[r][c] = "X"
+                elif board[r][c] == "T":
+                    board[r][c] = "O"
