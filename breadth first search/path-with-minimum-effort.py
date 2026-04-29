6b40b5e315541d79003e6b8a26433c06950daa84
SakshiG13arg <sakshigarg0913@gmail.com>
2026-04-29 12:42:25 +0530

    Implement minimumEffortPath method in Solution class

--- breadth first search/path-with-minimum-effort.py
+++ breadth first search/path-with-minimum-effort.py
@@ -1,1 +1,22 @@
-
+class Solution:
+    def minimumEffortPath(self, heights: List[List[int]]) -> int:
+        rows = len(heights)
+        cols = len(heights[0])
+        minHeap = [[0, 0, 0]]
+        visit = set()
+        directions = [[0, 1], [0, -1], [1, 0], [-1, 0]]
+        while minHeap:
+            diff, r, c = heapq.heappop(minHeap)
+            if (r, c) in visit:
+                continue
+            visit.add((r, c))
+            if (r, c) == (rows - 1, cols - 1):
+                return diff
+            for dr, dc in directions:
+                nr = r + dr
+                nc = c + dc
+                if (nr < 0 or nc < 0 or nr >= rows or nc >= cols or (nr , nc) in visit):
+                    continue
+                newDiff = max(diff, abs(heights[r][c] - heights[nr][nc]))
+                heapq.heappush(minHeap, [newDiff, nr, nc])
+        return 0
