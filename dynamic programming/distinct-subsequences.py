d6d31f173bf39938670cc82de32ae37c4c7de985
SakshiG13arg <sakshigarg0913@gmail.com>
2026-05-22 21:05:17 +0530

    Implement numDistinct method for distinct subsequences

--- dynamic programming/distinct-subsequences.py
+++ dynamic programming/distinct-subsequences.py
@@ -1,1 +1,14 @@
-
+class Solution:
+    def numDistinct(self, s: str, t: str) -> int:
+        m = len(s)
+        n = len(t)
+        dp = [0] * (n + 1)
+        for i in range(m - 1, -1, -1):
+            prev = 1
+            for j in range(n - 1, -1, -1):
+                res = dp[j]
+                if s[i] == t[j]:
+                    res += prev
+                prev = dp[j]
+                dp[j] = res
+        return dp[0]
