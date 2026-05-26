11a17e3e852ff827fbc8c50e61ce09a576ebb7b6
SakshiG13arg <sakshigarg0913@gmail.com>
2026-05-26 15:20:23 +0530

    Implement largest divisible subset algorithm

--- dynamic programming/largest-divisible-subset.py
+++ dynamic programming/largest-divisible-subset.py
@@ -1,1 +1,20 @@
-
+class Solution:
+    def largestDivisibleSubset(self, nums: List[int]) -> List[int]:
+        nums.sort()
+        n = len(nums)
+        dp = [[1, -1] for _ in range(n)]
+        max_len = 1
+        start_index = 0
+        for i in range(n):
+            for j in range(i):
+                if nums[i] % nums[j] == 0 and dp[j][0] + 1 > dp[i][0]:
+                    dp[i][0] = dp[j][0] + 1
+                    dp[i][1] = j
+            if dp[i][0] > max_len:
+                max_len = dp[i][0]
+                start_index = i
+        subset = []
+        while start_index != -1:
+            subset.append(nums[start_index])
+            start_index = dp[start_index][1]
+        return subset
