2460fa148a41dd0b021b167b6fa55e6b3adfb412
SakshiG13arg <sakshigarg0913@gmail.com>
2026-06-03 13:35:04 +0530

    Implement longest subarray with absolute diff limit

--- heap or priority queue/longest-common-subarray-with-absolute-diff-less-than-or-equal-to-limit.py
+++ heap or priority queue/longest-common-subarray-with-absolute-diff-less-than-or-equal-to-limit.py
@@ -1,1 +1,17 @@
-
+class Solution:
+    def longestSubarray(self, nums: List[int], limit: int) -> int:
+        maxHeap = []
+        minHeap = []
+        j = 0
+        res = 0
+        for i, v in enumerate(nums):
+            heapq.heappush(maxHeap, (-v, i))
+            heapq.heappush(minHeap, (v, i))
+            while -maxHeap[0][0] - minHeap[0][0] > limit:
+                j += 1
+                while maxHeap and maxHeap[0][1] < j:
+                    heapq.heappop(maxHeap)
+                while minHeap and minHeap[0][1] < j:
+                    heapq.heappop(minHeap)
+            res = max(res, i - j + 1)
+        return res
