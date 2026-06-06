09f82e80871a06c8270f21a9f14d4fad353d96c5
SakshiG13arg <sakshigarg0913@gmail.com>
2026-06-06 16:31:36 +0530

    Add putMarbles function to compute score difference
    Implement putMarbles function to calculate score difference.

--- heap or priority queue/put-marbles-in-bags.py
+++ heap or priority queue/put-marbles-in-bags.py
@@ -1,1 +1,19 @@
-
+class Solution:
+    def putMarbles(self, weights: List[int], k: int) -> int:
+        if k == 1:
+            return 0
+        min_heap = []
+        max_heap = []
+        for i in range(len(weights) - 1):
+            split = weights[i] + weights[i + 1]
+            if len(min_heap) < k - 1:
+                heapq.heappush(min_heap, split)
+            else:
+                heapq.heappushpop(min_heap, split)
+            if len(max_heap) < k - 1:
+                heapq.heappush(max_heap, -split)
+            else:
+                heapq.heappushpop(max_heap, -split)
+        max_score = sum(min_heap)
+        min_score = -sum(max_heap)
+        return max_score - min_score
