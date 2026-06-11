9851bc25a65867370793a3ffd9d9cee3e63ee6d9
SakshiG13arg <sakshigarg0913@gmail.com>
2026-06-11 14:45:24 +0530

    Implement findLucky function to find lucky integer

--- arrays and hashing/find-lucky-integer-in-an-array.py
+++ arrays and hashing/find-lucky-integer-in-an-array.py
@@ -1,1 +1,8 @@
-
+class Solution:
+    def findLucky(self, arr: List[int]) -> int:
+        cnt = Counter(arr)
+        res = -1
+        for num in cnt:
+            if num == cnt[num]:
+                res = max(num, res)
+        return res
