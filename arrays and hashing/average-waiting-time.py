cef7ce6c7330da9cf67931d2d2b053fff50c694e
SakshiG13arg <sakshigarg0913@gmail.com>
2026-06-15 12:51:35 +0530

    Implement average waiting time calculation

--- arrays and hashing/average-waiting-time.py
+++ arrays and hashing/average-waiting-time.py
@@ -1,1 +1,8 @@
-
+class Solution:
+    def averageWaitingTime(self, customers: List[List[int]]) -> float:
+        t = 0
+        total = 0
+        for arrival, order in customers:
+            t = max(t, arrival) + order
+            total += t - arrival
+        return total / len(customers)
