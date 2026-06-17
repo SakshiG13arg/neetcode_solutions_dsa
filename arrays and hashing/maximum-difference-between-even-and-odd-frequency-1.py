c4c0d477a2759750db6e0e2cce76696ab594fb2f
SakshiG13arg <sakshigarg0913@gmail.com>
2026-06-17 12:43:27 +0530

    Implement maxDifference method to calculate frequency difference

--- arrays and hashing/maximum-difference-between-even-and-odd-frequency-1.py
+++ arrays and hashing/maximum-difference-between-even-and-odd-frequency-1.py
@@ -1,1 +1,11 @@
-
+class Solution:
+    def maxDifference(self, s: str) -> int:
+        count = Counter(s)
+        oddMax = 0
+        evenMin = len(s)
+        for cnt in count.values():
+            if cnt & 1:
+                oddMax = max(oddMax, cnt)
+            else:
+                evenMin = min(evenMin, cnt)
+        return oddMax - evenMin
