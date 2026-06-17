f5e79839839077c4af9fe292d7aa030310daf21c
SakshiG13arg <sakshigarg0913@gmail.com>
2026-06-17 19:08:39 +0530

    Implement findDifference method in Solution class

--- arrays and hashing/find-the-difference-of-two-arrays.py
+++ arrays and hashing/find-the-difference-of-two-arrays.py
@@ -1,1 +1,13 @@
-
+class Solution:
+    def findDifference(self, nums1: List[int], nums2: List[int]) -> List[List[int]]:
+        num1Set = set(nums1)
+        num2Set = set(nums2)
+        res1 = []
+        res2 = []
+        for num in num1Set:
+            if num not in num2Set:
+                res1.append(num)
+        for num in num2Set:
+            if num not in num1Set:
+                res2.append(num)
+        return [res1, res2]
