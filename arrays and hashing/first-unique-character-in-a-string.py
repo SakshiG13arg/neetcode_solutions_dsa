5b1488f97fc5aa736a03339567aeb20d3b974751
SakshiG13arg <sakshigarg0913@gmail.com>
2026-06-18 13:25:44 +0530

    Implement first unique character finder in string

--- arrays and hashing/first-unique-character-in-a-string.py
+++ arrays and hashing/first-unique-character-in-a-string.py
@@ -1,1 +1,13 @@
-
+class Solution:
+    def firstUniqChar(self, s: str) -> int:
+        n = len(s)
+        count = defaultdict(int)
+        for i, c in enumerate(s):
+            if c not in count:
+                count[c] = i
+            else:
+                count[c] = n
+        res = n
+        for c in count:
+            res = min(res, count[c])
+        return -1 if res == n else res
