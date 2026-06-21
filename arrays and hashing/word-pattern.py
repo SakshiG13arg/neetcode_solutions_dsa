4403cbf934601da23456067db277a76384834c46
SakshiG13arg <sakshigarg0913@gmail.com>
2026-06-21 12:23:37 +0530

    Implement word pattern matching in Solution class

--- arrays and hashing/word-pattern.py
+++ arrays and hashing/word-pattern.py
@@ -1,1 +1,15 @@
-
+class Solution:
+    def wordPattern(self, pattern: str, s: str) -> bool:
+        words = s.split(" ")
+        if len(pattern) != len(words):
+            return False
+        charToWord = {}
+        wordToChar = {}
+        for c, w in zip(pattern, words):
+            if w in wordToChar and wordToChar[w] != c:
+                return False
+            if c in charToWord and charToWord[c] != w:
+                return False
+            charToWord[c] = w
+            wordToChar[w] = c
+        return True
