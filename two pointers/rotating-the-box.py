0eb07b910240dc1f4cd698155d1c02d0b6d9e942
SakshiG13arg <sakshigarg0913@gmail.com>
2026-06-27 13:16:01 +0530

    Implement rotateTheBox method for box rotation

--- two pointers/rotating-the-box.py
+++ two pointers/rotating-the-box.py
@@ -1,1 +1,15 @@
-
+class Solution:
+    def rotateTheBox(self, boxGrid: List[List[str]]) -> List[List[str]]:
+        rows = len(boxGrid)
+        cols = len(boxGrid[0])
+        res = [["."] * rows for _ in range(cols)]
+        for r in range(rows):
+            i = cols - 1
+            for c in reversed(range(cols)):
+                if boxGrid[r][c] == "#":
+                    res[i][rows - r - 1] = "#"
+                    i -= 1
+                elif boxGrid[r][c] == "*":
+                    res[c][rows - r - 1] = "*"
+                    i = c - 1
+        return res
