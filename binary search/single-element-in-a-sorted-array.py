4d86d36949cb24bea9394e3436caa04b0c4ae352
SakshiG13arg <sakshigarg0913@gmail.com>
2026-07-02 18:46:06 +0530

    Implement binary search to find single element

--- binary search/single-element-in-a-sorted-array.py
+++ binary search/single-element-in-a-sorted-array.py
@@ -1,1 +1,13 @@
-
+class Solution:
+    def singleNonDuplicate(self, nums: List[int]) -> int:
+        l = 0
+        r = len(nums) - 1
+        while l < r:
+            m = l + ( r - l) // 2
+            if m & 1:
+                m -= 1
+            if nums[m] != nums[m + 1]:
+                r = m
+            else:
+                l = m + 2
+        return nums[l]
