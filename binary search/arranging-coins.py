2346b502ef73efa6b4c8017f8c6117542c4894b9
SakshiG13arg <sakshigarg0913@gmail.com>
2026-07-02 12:17:32 +0530

    Implement binary search for arranging coins

--- binary search/arranging-coins.py
+++ binary search/arranging-coins.py
@@ -1,1 +1,13 @@
-
+class Solution:
+    def arrangeCoins(self, n: int) -> int:
+        if n <= 3:
+            return n if n == 1 else n - 1
+        l = 1
+        r = (n // 2) + 1
+        while l < r:
+            mid = (l + r) // 2
+            if (mid * (mid + 1)) // 2 <= n:
+                l = mid + 1
+            else:
+                r = mid
+        return l - 1
