15d9a1b22329c4c9979b696789f5088a3cccc3ca
SakshiG13arg <sakshigarg0913@gmail.com>
2026-07-05 13:21:21 +0530

    Add largestSubmatrix method to Solution class
    Implement the largestSubmatrix method to find the largest submatrix with rearrangements.

--- greedy/largest-submatrix-with-rearrangements.py
+++ greedy/largest-submatrix-with-rearrangements.py
@@ -1,1 +1,14 @@
-
+class Solution:
+    def largestSubmatrix(self, matrix: List[List[int]]) -> int:
+        rows = len(matrix)
+        cols = len(matrix[0])
+        res = 0
+        for r in range(1, rows):
+            for c in range(cols):
+                if matrix[r][c]:
+                    matrix[r][c] += matrix[r - 1][c]
+        for r in range(rows):
+            matrix[r].sort(reverse = True)
+            for i in range(cols):
+                res = max(res, (i + 1) * matrix[r][i])
+        return res
