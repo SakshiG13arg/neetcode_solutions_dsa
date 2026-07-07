f7bb2710b3cc8b962482e19efb39143fd8c8639c
SakshiG13arg <sakshigarg0913@gmail.com>
2026-07-07 18:13:46 +0530

    Implement sumAndMultiply method in Solution class

--- two pointers/concatenate-non-zero-digits-and-multiply-by-sum-1.py
+++ two pointers/concatenate-non-zero-digits-and-multiply-by-sum-1.py
@@ -1,1 +1,14 @@
-
+class Solution(object):
+    def sumAndMultiply(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        x = 0
+        n_sum = 0
+        for i in str(n):
+            digit = int(i)
+            if digit != 0:
+                x = x * 10 + digit
+                n_sum += digit
+        return x * n_sum
