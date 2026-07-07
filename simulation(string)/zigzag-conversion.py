e5ccde16d0a2bc7d23580f0a153aea88b70648db
SakshiG13arg <sakshigarg0913@gmail.com>
2026-07-07 12:10:29 +0530

    Implement zigzag conversion algorithm

--- simulation(string)/zigzag-conversion.py
+++ simulation(string)/zigzag-conversion.py
@@ -1,1 +1,13 @@
-
+class Solution:
+    def convert(self, s: str, numRows: int) -> str:
+        if numRows == 1 or numRows >= len(s):
+            return s
+        res = [[] for _ in range(numRows)]
+        row = 0
+        dir = 1
+        for c in s:
+            res[row].append(c)
+            row += dir
+            if row == 0 or row == (numRows - 1):
+                dir *= -1
+        return ''.join([''.join(row)for row in res])
